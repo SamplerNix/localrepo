5b42bb79962ede0f8eda391a586329e76501221e
SamplerNix <nikhilsingh7534@gmail.com>
2025-02-22 19:03:47 +0530

    added function overloading

--- college/assgnment2.cpp
+++ college/assgnment2.cpp
@@ -24,9 +24,27 @@ class student{
         cout<<name<<"\n"<<age<<"\n"<<grade<<endl;
     }
 };
+class functionoverloading{
+    public:
+void add(int x){
+    int b=x++;
+    cout<<"when number added to it's increament"<<x+b;
+}
+void add(float x,int a){
+    float result=x+a;
+    cout<<"This will print floating and integer sum"<<result<<endl;
+}
+void add(float x,float y){
+    float sum=x+y;
+    cout<<"sum of two floating numbe is :"<<sum<<endl;
+}
+};
+
 int main(){
     string a="Hackor";
     myclass obj(2);
     student obj1(a,21,'A');
+    functionoverloading obj2;
+    obj2.add(2.3,5);
     return -1;
 }
